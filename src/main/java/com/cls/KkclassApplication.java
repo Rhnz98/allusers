6930228947b8631dfb5b1ec3bf56baa4d8367377
MAHARJAN ROHIT <c200524@ccg.ac.jp>
2022-05-31 18:21:27 +0900

    error

--- src/main/java/com/cls/KkclassApplication.java
+++ src/main/java/com/cls/KkclassApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KkclassApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KkclassApplication.class, args);
+		SpringApplication.run(KkclassApplication.class, ar);
 		
 		
 		
