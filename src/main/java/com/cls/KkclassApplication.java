ff596426b604a16405bd933431e39a766fa23608
MAHARJAN ROHIT <c200524@ccg.ac.jp>
2022-05-31 17:23:37 +0900

    first commit

---
+++ src/main/java/com/cls/KkclassApplication.java
@@ -0,0 +1,13 @@
+package com.cls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cls/KkclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
